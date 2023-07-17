47266f01eb33a04ecf8ebc1b1f73507b8443f7ce
Xianpi Duan <97914968+duanxianpi@users.noreply.github.com>
2023-07-17 09:40:26 +0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -9,8 +9,16 @@ df = pd.read_csv('stats.csv')
 # Convert the 'Timestamp' column to datetime format
 df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='s') - pd.Timedelta('05:00:00')
 
-# Get last 7 days of the data
-df['Timestamp'] =  df[df['Timestamp'] > df['Timestamp'].max() - pd.Timedelta(days=5)]["Timestamp"]
+# Get last 5 days of the data
+df =  df[df['Timestamp'] > df['Timestamp'].max() - pd.Timedelta(days=5)]
+
+plot_names = []
+for name,group in df.groupby('Name')['Runs']:
+    if group.max() > group.min():
+        plot_names.append(name)
+
+# Get the rows with the algorithms in plot_names
+df = df[df['Name'].isin(plot_names)]
 
 df.sort_values(['Timestamp', 'Name'], inplace=True)
 fig, ax = plt.subplots(figsize=(16,9))
