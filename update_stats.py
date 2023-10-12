4f2ada271ce68d0ea4952bfa3e7cb1242dbe411c
duanxianpi <duanxianpi@gmail.com>
2023-10-12 02:01:40 -0400

    update

--- update_stats.py
+++ update_stats.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests as re
 import time
+import os
 
 currentYearMonth = time.strftime("%Y-%m")
 
@@ -12,13 +13,16 @@ soup = BeautifulSoup(html, 'html.parser')
 
 all_alogrithms = soup.select('div.row.row-cols-1.row-cols-md-2.row-cols-lg-4.g-4.card-group > div')
 
+isStatsExist = os.path.exists(f"stats/stats-{currentYearMonth}.csv")
+
 with open(f'stats/stats-{currentYearMonth}.csv', 'a') as f:
+    if not isStatsExist:
+        f.write("Timestamp,Name,Author,Ups,Runs\n")
     for alogrithm in all_alogrithms:
         name = alogrithm.select_one('div > div.card-body > h5').text.strip()
         author = alogrithm.select_one('div > div.card-footer > p').text.strip()[13:]
         ups = alogrithm.select_one('div > div.card-body > div > div:nth-child(1) > a').text.strip()
         runs = alogrithm.select_one('div > div.card-body > div > div:nth-child(2) > a').text.strip()
         
-        row = '{},"{}","{}",{},{}\n'.format(now ,name, author, ups, runs)
-        f.write(row)
+        f.write(f'{now},"{name}","{author}",{ups},{runs}\n')
 
